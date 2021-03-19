8188951556232481bd9bd84dbacbe199954fa665
= <=>
2021-03-19 21:57:59 +0700

    19/3/2021

--- src/public/js/tabUi.js
+++ src/public/js/tabUi.js
@@ -10,12 +10,9 @@
  line.style.left = tabActive.offsetLeft + 'px';
  line.style.width = tabActive.offsetWidth + 'px';
 
-
  tabs.forEach((tab, index) => {
-
      const pane = panes[index]
      tab.onclick = function() {
-
          $('.tab-item.active').classList.remove('active');
          $('.tab-pane.active').classList.remove('active');
          line.style.left = this.offsetLeft + 'px';
