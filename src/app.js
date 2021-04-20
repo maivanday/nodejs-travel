1b5e3dbf77f61d60e269d3d9027ad2ac8fb5d21c
= <=>
2021-04-20 21:05:32 +0700

    fix port

--- src/app.js
+++ src/app.js
@@ -55,6 +55,6 @@ app.use(express.json());
 // router
 route(app);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(` App listening at http://localhost:${port}`)
 })
